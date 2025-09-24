19d8633b98e8b034c48cdadbc29ad507a472bfdd
25021977-source <25021977@vnu.edu.vn>
2025-09-24 21:44:28 +0700

    Update Homework1.py

--- Homework1.py
+++ Homework1.py
@@ -1,3 +1,9 @@
-n = int(input("Nhap so nguyen n la :"))
-A = 2*n
-print("So gap doi cua n la:",A)
+#hw24
+a = float(input("Nhap chieu rong cua hcn la:"))
+b = float(input("nhap chieu dai cua hcn la:"))
+if(0 < a < b):
+  shcn = a * b
+  stc = shcn - 157/50 *((a/2)**2)
+  print(f'Dien tich trong cay xung quanh la:{stc:.2f}')
+else :
+  print("Khong thoa man ycbt")
