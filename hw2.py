df1e05b28697c125d96cb63a92481b65d5ae8bba
25021977-source <25021977@vnu.edu.vn>
2025-10-01 17:42:32 +0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,25 +1,13 @@
 #hw2
-#Bài 6: Viết chương trình giải phương trình bậc 2 (ax2 + bx + c = 0)
-a , b , c = map(float,input().split())
-if a == 0 :
-# phuong trinh tro thanh b * x + c = 0
-  if c == b == 0 :
-    print("Phuong trinh co vo so nghiem")
-  elif b == 0 and c != b :
-    print("Phuong trinh da cho vo nghiem")
-  else :
-    x = -c / b 
-    print("Phuong trinh da cho co nghiem la:",x)
+#Bài 9: Giải hệ phương trình tuyến tính:
+#ax + by = m
+#cx + dy = n
+a , b , c , d , m , n = map(float,input().split())
+if a * d - b * c == 0 : 
+  print("He phuong trinh vo nghiem")
 else : 
-  delta = b ** 2 - 4 * a * c
-  if delta < 0 :
-    print("Phuong trinh da cho vo nghiem")
-  elif delta == 0 :
-    x = -b / (2 * a )
-    print("Phuong trinh co nghiem duy nhat la :",x)
-  else :
-    x1 = (-b + delta ** 0.5 ) / (2 * a )
-    x2 = (-b - delta ** 0.5 ) / ( 2 * a )
-    print("Phuong trinh co 2 nghiem phan biet la:")
-    print("x1 = ",x1)
-    print("x2 = ",x2)
+   x = (m * d - b * n ) / (a * d - b * c )
+   y = (a * n - c * m ) / (a * d - b * c )
+   print("He phuong trinh co nghiem la :")
+   print("x = ",x)
+   print("y = ",y)
