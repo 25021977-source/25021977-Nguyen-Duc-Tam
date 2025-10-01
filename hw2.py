2facdf50df12ee78368c6e719ced31722720ce87
25021977-source <25021977@vnu.edu.vn>
2025-10-01 20:45:45 +0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,13 +1,7 @@
 #hw2
-#Bài 9: Giải hệ phương trình tuyến tính:
-#ax + by = m
-#cx + dy = n
-a , b , c , d , m , n = map(float,input().split())
-if a * d - b * c == 0 : 
-  print("He phuong trinh vo nghiem")
-else : 
-   x = (m * d - b * n ) / (a * d - b * c )
-   y = (a * n - c * m ) / (a * d - b * c )
-   print("He phuong trinh co nghiem la :")
-   print("x = ",x)
-   print("y = ",y)
+#Bài 10: Nhập vào thời gian 1 công việc nào đó là x giây. Hãy chuyển đổi và viết ra màn hình số thời gian trên dưới dạng bằng bao nhiêu giờ, bao nhiêu phút, bao nhiêu giây. 
+x = int(input("Nhap so giay la:"))
+so_phut = x / 60
+so_gio = x / 3600
+print("SO phut la:",so_phut)
+print("SO gio la:",so_gio)
