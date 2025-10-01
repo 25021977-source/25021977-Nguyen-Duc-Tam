e5d4dea30e5ee6d05307bcbc90de38038b6c4975
25021977-source <25021977@vnu.edu.vn>
2025-10-01 12:13:45 +0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,6 +1,7 @@
-#Bài 1: Nhập vào 2 cạnh của một hình chữ nhật. In ra màn hình diện tích và chu vi của nó.
-a , b = map(float,input().split())
-C = (a + b) * 2
-S = a * b
-print("Chu vi hinh chu nhat la:",C)
-print("Dien tich hinh chu nhat la:",S)
+#Bài 2: Nhập vào bán kính của hình tròn. In ra màn hình diện tích và chu vi của nó.
+pi = 3.14
+r = float(input("Nhap ban kinh cua hinh tron :"))
+C = 2 * pi * r
+S = pi * r * r
+print("Chu vi hinh tron la:",C)
+print("Dien tich hinh tron la:",S)
