ea986b632049dae08963ac6c846e28c81f17e5c2
25021977-source <25021977@vnu.edu.vn>
2025-10-01 16:55:49 +0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,5 +1,10 @@
 #hw2
-#Bài 8: Tìm giá trị nhỏ nhất trong 4 số a, b, c, d (a, b, c, được nhập từ bàn phím). 
-a , b , c , d =  map(float,input().split())
-min_value = min(a , b , c , d)
-print("gia tri nho nhat la:",min_value)
+#Bài 4: Viết chương trình giải phương trìBnh bậc 1 (ax + b = 0)
+a , b = map(float,input().split())
+if a == b == 0 :
+  print("Phuong trinh co vo so nghiem")
+elif a == 0 and a != b :
+  print("Phuong trinh da cho vo nghiem")
+else :
+  x = -b / a 
+  print("Phuong trinh da cho co nghiem la:",x)
