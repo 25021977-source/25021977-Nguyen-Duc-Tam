e56bbb00164217d8b8a9df09802c1bea53f3ca93
25021977-source <25021977@vnu.edu.vn>
2025-10-01 21:06:42 +0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,5 +1,9 @@
 #hw2
-#Bài 12: Tính x**y ( Với x, y là số thực). 
-x = float(input("NHap so thuc x la:"))
-y = float(input("NHap so thuc y la:"))
-print("x**y = ",x ** y)
+#Bài 11: Nhập vào tâm và bán kính của một đường tròn. Sau đó nhập vào một điểm A(x, y) bất kì và kiểm tra xem nó có thuộc đường tròn hay không?
+xo , yo = map(float,(input("Nhap toa do tam cua duong tron I(xo , yo):")).split())
+R = float(input("Nhap ban kinh duong tron la :"))
+x , y = map(float,(input("Nhap toa do diem A(x , y)")).split())
+if (x - xo)**2 + (y - yo)**2 == R**2 :
+  print("Diem A thuoc duong tron tam I")
+else :
+  print("Diem A khong thuoc duong tron tam I")
