0fd48138e3a20a1e70a4bc2e41ab867a66803818
25021977-source <25021977@vnu.edu.vn>
2025-10-01 16:43:59 +0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,5 +1,5 @@
 #hw2
-#Bài 7: Tìm giá trị lớn nhất trong 4 số a, b, c, d (a, b, c, được nhập từ bàn phím). 
+#Bài 8: Tìm giá trị nhỏ nhất trong 4 số a, b, c, d (a, b, c, được nhập từ bàn phím). 
 a , b , c , d =  map(float,input().split())
-max_value = max(a , b , c , d)
-print("gia tri lon nhat la:",max_value)
+min_value = min(a , b , c , d)
+print("gia tri nho nhat la:",min_value)
