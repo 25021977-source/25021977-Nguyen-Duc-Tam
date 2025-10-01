175d3aacbb7dbb5d13888e52423ba3f7e24f40c4
25021977-source <25021977@vnu.edu.vn>
2025-10-01 20:50:22 +0700

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,7 +1,5 @@
 #hw2
-#Bài 10: Nhập vào thời gian 1 công việc nào đó là x giây. Hãy chuyển đổi và viết ra màn hình số thời gian trên dưới dạng bằng bao nhiêu giờ, bao nhiêu phút, bao nhiêu giây. 
-x = int(input("Nhap so giay la:"))
-so_phut = x / 60
-so_gio = x / 3600
-print("SO phut la:",so_phut)
-print("SO gio la:",so_gio)
+#Bài 12: Tính x**y ( Với x, y là số thực). 
+x = float(input("NHap so thuc x la:"))
+y = float(input("NHap so thuc y la:"))
+print("x**y = ",x ** y)
