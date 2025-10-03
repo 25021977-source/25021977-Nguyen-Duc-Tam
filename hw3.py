c5a9385e1f004efe954a069786520655e7b2a70b
25021977-source <25021977@vnu.edu.vn>
2025-10-03 18:56:35 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,12 +1,17 @@
-#w3A13
-#[Giải phương trình bậc nhất] 
-#Nhập vào hai số thực a và b của phương trình ax + b = 0.
-#Tìm và in ra nghiệm của phương trình, kết quả làm tròn đến số thập phân thứ 2. In ra "Vô nghiệm" hoặc "Vô số nghiệm" trong trường hợp tương ứng
-a , b = map(float,input().split())
-if a == b == 0 :
-  print("Vo sO nghiem")
-elif a == 0 and b != 0 :
-  print("Vo nghiem")
+#3A14
+#Xếp loại học lực] 
+#Nhập vào điểm trung bình của một học sinh và in ra học lực của học sinh đó. 
+#Xếp loại học lực theo quy tắc: 
+#- >= 8.0: Giỏi
+#- >= 6.5: Khá 
+#- >= 5.0: Trung bình 
+#- < 5.0: Yếu
+n = float(input("Nhap diem trung binh:"))
+if n >= 8 :
+  print("Gioi")
+elif n >= 6.5 :
+  print("Kha")
+elif n >= 5 :
+  print("Trung binh")
 else :
-  x = -b / a 
-  print(f"Phuong trinh co nghiem :x = {x:.2f}")
+  print("Yeu")
