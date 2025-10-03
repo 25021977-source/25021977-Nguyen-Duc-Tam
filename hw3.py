b285be5137d0abd21bd7486a8a25869e01f21d9e
25021977-source <25021977@vnu.edu.vn>
2025-10-03 18:27:00 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,9 +1,6 @@
-#w3A9
-#[Triangle] 
-#Nhập vào 3 số nguyên dương a, b, c.
-#Kiểm tra xem a, b, c có cấu thành độ dài cạnh của 1 tam giác được không. Nếu a, b, c cấu tạo thành được một tam giác, in ra "Yes". Ngược lại in ra "No".
-a , b ,c = map(float,input().split())
-if a + b > c and a +  c > b and b + c > a :
-  print("Yes")
-else :
-  print("No")
+#w3A10
+#[Largest number] 
+#Nhập vào 4 số nguyên. In ra màn hình số nguyên lớn nhất trong 4 số
+a , b ,c ,d = map(int,input().split())
+max_value = max(a , b , c , d)
+print(max_value)
