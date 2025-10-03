753a30b99b5424e6b9625626db80c3ac3a9334fa
25021977-source <25021977@vnu.edu.vn>
2025-10-03 18:39:51 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,17 +1,9 @@
-#w3A11
-#Phân loại tam giác] 
-#Nhập vào 3 số nguyên dương a,b,c là độ dài 3 cạnh của một tam giác. Kiểm tra 3 số đã nhập có tạo thành một tam giác hợp lệ hay không.
-#Nếu không in ra "Không phải tam giác". Nếu có, phân loại tam giác đó là "Tam giác đều", "Tam giác cân", hay "Tam giác thường".
-a , b , c = map(int,input().split())
-if a + b > c and a + c > b and b + c > a :
-  if a == b == c :
-    print("Tam giac deu")
-  elif a == b or a == c or b ==c :
-    print("Tam giac can")
-  elif a ** 2 + b ** 2 == c ** 2 or a ** 2 + c ** 2 == b ** 2 or b ** 2 + c ** 2 == a ** 2 :
-    print("Tam giac vuong")
-  else :
-    print("Tam giac thuong ")
-else  :
-  print("Khong phai tam giac")
-  
+#w3A12
+#Kiểm tra năm nhuận] 
+#Nhập vào một số nguyên dương n là số năm, kiểm tra xem năm đó có phải là năm nhuận hay không. 
+#Năm nhuận là năm chia hết cho 4 nhưng không chia hết cho 100, hoặc chia hết cho 400. Nếu là năm nhuận in ra "Yes", nếu không in ra "No"
+n = int(input())
+if (n % 4 == 0 and n % 100 != 0) or (n % 400 == 0):
+  print("Yes")
+else : 
+  print("No")
