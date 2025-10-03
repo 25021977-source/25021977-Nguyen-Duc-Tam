1f306a416cbc388d8990185df91a6746719479e1
25021977-source <25021977@vnu.edu.vn>
2025-10-03 18:05:23 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,6 +1,6 @@
-#w3A4
-#Viêt chương trình chia một số m cho n, kết quả thu được làm tròn xuống
+#w3A5
+#Viêt chương trình chia một số m cho n, kết quả thu được làm tròn lên
 import math
 m , n = map(float,input().split())
 x = m / n
-print(math.floor(x))
+print(math.ceil(x))
