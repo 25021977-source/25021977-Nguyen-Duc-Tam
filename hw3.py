e60d09430e2140baa6aaf06ff06f0002f783d314
25021977-source <25021977@vnu.edu.vn>
2025-10-03 17:56:15 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,9 +1,5 @@
-#w3A3
-#Kiểm tra xem một số có phải là lũy thừa của 2 hay không 
-#Sử dụng các toán tử bitwise để xác định xem một số có phải là lũy thừa của 2 hay không. 
-#Gợi ý: Đối với lũy thừa của 2, n & (n-1) == 0
-n = int(input())
-if  n > 0 and n & (n -1 ) == 0:
-  print("TRUE")
-else :
-  print("FALSE")
+#w3A4
+#Viêt chương trình chia một số m cho n, kết quả thu được làm tròn xuống
+m , n = map(float,input().split())
+x = m // n
+print(int(x))
