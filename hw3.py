b0a37285fea03a99ddacd687505f764a314c8188
25021977-source <25021977@vnu.edu.vn>
2025-10-03 18:49:48 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,9 +1,12 @@
-#w3A12
-#Kiểm tra năm nhuận] 
-#Nhập vào một số nguyên dương n là số năm, kiểm tra xem năm đó có phải là năm nhuận hay không. 
-#Năm nhuận là năm chia hết cho 4 nhưng không chia hết cho 100, hoặc chia hết cho 400. Nếu là năm nhuận in ra "Yes", nếu không in ra "No"
-n = int(input())
-if (n % 4 == 0 and n % 100 != 0) or (n % 400 == 0):
-  print("Yes")
-else : 
-  print("No")
+#w3A13
+#[Giải phương trình bậc nhất] 
+#Nhập vào hai số thực a và b của phương trình ax + b = 0.
+#Tìm và in ra nghiệm của phương trình, kết quả làm tròn đến số thập phân thứ 2. In ra "Vô nghiệm" hoặc "Vô số nghiệm" trong trường hợp tương ứng
+a , b = map(float,input().split())
+if a == b == 0 :
+  print("Vo sO nghiem")
+elif a == 0 and b != 0 :
+  print("Vo nghiem")
+else :
+  x = -b / a 
+  print(f"Phuong trinh co nghiem :x = {x:.2f}")
