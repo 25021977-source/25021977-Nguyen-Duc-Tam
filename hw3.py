3a07f90670d7fd2b82a30ca1311907157db04258
25021977-source <25021977@vnu.edu.vn>
2025-10-03 19:29:10 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,17 +1,13 @@
-#3A15
-#Xếp loại học lực] 
-#Nhập vào điểm trung bình của một học sinh và in ra học lực của học sinh đó. 
-#Xếp loại học lực theo quy tắc: 
-#- >= 8.0: Giỏi
-#- >= 6.5: Khá 
-#- >= 5.0: Trung bình 
-#- < 5.0: Yếu
-n = float(input("Nhap diem trung binh:"))
-if n >= 8 :
-  print("Gioi")
-elif n >= 6.5 :
-  print("Kha")
-elif n >= 5 :
-  print("Trung binh")
+#w3A17
+#Cấp số nhân 
+#Cho 4 số a, b, c, d. Hãy kiểm tra xem 4 số này có thể theo thứ tự tạo thành 1 cấp số nhân với công bội nguyên theo đúng thứ tự a, b, c, d hay không? 
+#Gợi ý : Tìm công bội (b / a) nếu b chia hết cho a, sau đó lấy b nhân công bội và so sánh vs c, c nhân công bội và so sánh vs d
+a , b , c , d = map(float,input().split())
+if a != 0 and b % a == 0:
+   x = b // a 
+   if c * x == d and b * x == c and a * x == b:
+      print(" La cap so nhan")
+   else :
+      print("Khong phai cap so nhan")
 else :
-  print("Yeu")
+  print("Khong phai cap so nhan")
