5881ff58f0237c7f0f7d417e57cb5d9a922247fe
25021977-source <25021977@vnu.edu.vn>
2025-10-08 12:01:30 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,10 +1,8 @@
-#Bài 7: Nhập vào số nguyên dương a, đếm số ước dương của a 
-n = int(input())
-if n > 0:
-  count = 0
-  for i in range(1 , n+1):
-    if n  % i == 0 :
-      count += 1
-  print(count)
+#Bài 8: Nhập vào số nguyên dương a và b, in toàn bộ ước dương chung của a và b 
+a , b = map(int,input().split())
+if a > 0 and b > 0:
+  for i in range(1 ,a + 1):
+    if a % i == 0 and b % i == 0  :
+      print(i)
 else :
-  print('Vui long nhap so nguyen duong')
+  print("Vui long nhap so nguyen duong")
