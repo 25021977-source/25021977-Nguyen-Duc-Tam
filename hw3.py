6f6a7359d4fdedba019d968b8a4d5a44be3afb70
25021977-source <25021977@vnu.edu.vn>
2025-10-08 11:47:22 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,8 +1,10 @@
-#Bài 6: Nhập vào số nguyên dương a, in ra tất cả các ước dương của a 
+#Bài 7: Nhập vào số nguyên dương a, đếm số ước dương của a 
 n = int(input())
 if n > 0:
+  count = 0
   for i in range(1 , n+1):
-    if n % i == 0 :
-      print(i)
+    if n  % i == 0 :
+      count += 1
+  print(count)
 else :
   print('Vui long nhap so nguyen duong')
