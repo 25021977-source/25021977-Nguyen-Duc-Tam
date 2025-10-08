f1681a78c310c0a260d3be395a5851220d854ad6
25021977-source <25021977@vnu.edu.vn>
2025-10-08 11:17:46 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,6 +1,6 @@
-#Bài 1: In các số dương lẻ bé hơn 100 
+#Bài 2: In các số dương chẵn chia hết cho 3 bé hơn 100 
 n = 100
 i = 1
 for i in range(1 , 100):
-  if i %2 != 0 :
+  if i % 6 == 0 :
     print(i)
