0d00ea587d17be1bb84aeff2d84233ac8775a495
25021977-source <25021977@vnu.edu.vn>
2025-10-08 11:27:20 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,6 +1,8 @@
-#Bài 2: In các số dương chẵn chia hết cho 3 bé hơn 100 
-n = 100
-i = 1
-for i in range(1 , 100):
-  if i % 6 == 0 :
-    print(i)
+#Bài 3: Nhập vào số nguyên dương a, in ra bảng cửu chương của a 
+n = int(input())
+if n > 0:
+  i = 1
+  for i in range(1 , 11):
+    print(f'{n} * {i} = {n * i}')
+else :
+  print("vui long nhap so nguyen duong")
