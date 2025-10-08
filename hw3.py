775dbec76c75944ff6c44301ed1ae5af97704ef5
25021977-source <25021977@vnu.edu.vn>
2025-10-08 19:39:26 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,8 +1,10 @@
-#Bài 8: Nhập vào số nguyên dương a và b, in toàn bộ ước dương chung của a và b 
-a , b = map(int,input().split())
-if a > 0 and b > 0:
-  for i in range(1 ,a + 1):
-    if a % i == 0 and b % i == 0  :
-      print(i)
+#Bài 9: Nhập vào số nguyên dương a, kiểm tra xem a có phải là số chính phương hay không? Số chính phương là số có căn bậc hai là một số nguyên VD: 1, 4, 9, 16, 25…
+import math
+a = int(input())
+if a > 0:
+  if math.isqrt(a) ** 2 == a :
+    print(f'{a} la so chinh phuong')
+  else :
+    print(f'{a} khong la so chinh phuong')
 else :
-  print("Vui long nhap so nguyen duong")
+  print('Vui long nhap so nguyen duong')
