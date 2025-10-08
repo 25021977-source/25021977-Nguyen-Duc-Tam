7ad68663b9cf2d87d3f2413fbd9a56714e23288a
25021977-source <25021977@vnu.edu.vn>
2025-10-08 21:16:12 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,10 +1,14 @@
-#Bài 9: Nhập vào số nguyên dương a, kiểm tra xem a có phải là số chính phương hay không? Số chính phương là số có căn bậc hai là một số nguyên VD: 1, 4, 9, 16, 25…
-import math
+#Bài 10: Nhập vào số nguyên dương a
+#kiểm tra xem a có phải là số hoàn hảo hay không. Số hoàn hảo là số có tổng các ước (ước nhỏ hơn nó) bằng chính nó, VD: số 6, 28… 
 a = int(input())
 if a > 0:
-  if math.isqrt(a) ** 2 == a :
-    print(f'{a} la so chinh phuong')
+  sum = 0
+  for i in range (1 , a ):
+    if a % i == 0 :
+      sum += i
+  if sum == a :
+    print(f'{a} la so hoan hao')
   else :
-    print(f'{a} khong la so chinh phuong')
+    print(f'{a} khong la so hoan hao')
 else :
-  print('Vui long nhap so nguyen duong')
+  print("Vui long nhap so nguyen duong ")
