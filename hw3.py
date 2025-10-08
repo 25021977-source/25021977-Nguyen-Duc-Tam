045fe911e86967643396cae55f57c0a61f19ea43
25021977-source <25021977@vnu.edu.vn>
2025-10-08 22:28:58 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,10 +1,12 @@
-#Bài 11: Nhập vào số nguyên a
-#Nếu nhập số âm thì yêu cầu nhập lại cho đến khi người dùng nhập đúng số dương. 
-#Nếu người dùng nhập đúng số dương thì in ra “Bạn nhập đúng quy tắc” và dừng chương trình. 
-while True :
-  a = int(input())
-  if a > 0:
-    print("Ban nhap sung quy tac")
-    break
-  else :
-    print("Vui long nhap so nguyen duong")
+#Bài 12: Nhập số nguyên dương n, Cho S(k) = 1 + 2 + 3 + … + k. Tìm k sao cho S(k) lớn nhất nhưng nhỏ hơn n 
+n = int(input("n = ")) 
+if n > 0:
+  sum = 0
+  k = 0
+  while sum + (k +1)  < n:
+    k += 1
+    sum += k
+  print(f'k = {k}')
+else :
+  print("Vui long nhap so nguyen duong")
+  
