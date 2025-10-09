b84917aa1c4ba3e52bda3ff4fb7b79122df041d9
25021977-source <25021977@vnu.edu.vn>
2025-10-09 10:11:53 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,10 +1,10 @@
-#Bài 13: Nhập vào số nguyên dương A, tìm n nhỏ nhất sao cho 
-# 1 + 1/2 + 1/3 + 1/4 + ... + 1/n > A 
-A = int(input(f'A = '))
-if A > 0 :
-  sum = 0
-  n = 1
-  while sum <= A:
-    sum += 1 / n
-    n += 1 
-  print(f'n = {n -1}')
+#Bài 11: Nhập vào số nguyên a
+#Nếu nhập số âm thì yêu cầu nhập lại cho đến khi người dùng nhập đúng số dương.
+#Nếu người dùng nhập đúng số dương thì in ra “Bạn nhập đúng quy tắc” và dừng chương trình.
+while True :
+  a = int(input())
+  if a > 0:
+    print("Ban nhap dung quy tac")
+    break
+  else :
+    print("Vui long nhap so nguyen duong")
