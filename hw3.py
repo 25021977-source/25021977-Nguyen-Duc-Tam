92e1d273543e9b3a88f4176dd50ced7c4b03e061
25021977-source <25021977@vnu.edu.vn>
2025-10-09 11:25:06 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,10 +1,9 @@
 #Bài 15: Nhập vào số nguyên dương n, đếm xem n có bao nhiêu chữ số. Làm bằng nhiều cách nhất có thể, ko dùng mảng. 
 n = int(input())
 if n > 0 :
- count = 0
- while n > 0:
-   n //= 10
-   count += 1 
- print(count)
+  count = 0
+  while 10 ** count <= n :
+    count += 1 
+  print(count)
 else :
   print("Vui long nhap so nguyen duong")
