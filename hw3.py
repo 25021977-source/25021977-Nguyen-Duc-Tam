0057d25acfd730543cac9590b282a406fab9d112
25021977-source <25021977@vnu.edu.vn>
2025-11-24 23:08:52 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,16 +1,10 @@
-#Bài 16: Nhập vào số nguyên dương n, đếm xem n có bao nhiêu chữ số chẵn, bao nhiêu chữ số lẻ
+#Bài 17: Nhập vào số nguyên dương n, tính tổng các chữ số của n
 n = int(input())
-if n > 0:
-  count = 0
-  count_1 = 0
-  while n > 0 :
-    a = n % 10
-    if a % 2 == 0 :
-      count += 1 
-    else :
-      count_1 += 1
+if n > 0 :
+  sum = 0
+  while n > 0:
+    sum += n % 10
     n //= 10
-  print(f'{count} so chan')
-  print(f'{count_1} so le')
+  print(sum)
 else :
-  print("vui long nhap so nguyen duong")
+  print("Vui long nhap so nguyen duong")
