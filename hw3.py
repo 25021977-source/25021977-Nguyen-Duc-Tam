aecca6793198a7d9d9690403ee9c5a494abd9640
25021977-source <25021977@vnu.edu.vn>
2025-11-24 23:13:50 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,10 +1,11 @@
-#Bài 17: Nhập vào số nguyên dương n, tính tổng các chữ số của n
+#Bài 18: Nhập vào một số nguyên dương n, kiểm tra xem n có phải là số dạng 3^k hay không
 n = int(input())
-if n > 0 :
-  sum = 0
-  while n > 0:
-    sum += n % 10
-    n //= 10
-  print(sum)
+if n > 0:
+  while n % 3 == 0:
+    n //= 3
+  if n == 1 :
+      print("So dang 3^k")
+  else :
+      print("Khong phai")
 else :
   print("Vui long nhap so nguyen duong")
