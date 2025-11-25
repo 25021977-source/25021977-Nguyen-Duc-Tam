4f44cfdb1f85efcb70346bf161f4f247d4c0dbf6
25021977-source <25021977@vnu.edu.vn>
2025-11-25 07:41:04 +0700

    Update btt4.py

--- btt4.py
+++ btt4.py
@@ -1,9 +1,25 @@
-#w4A1
-#[SumOfNumBers]
-#Viết chương trình Python sử dụng một trong 2 loại vòng lặp for hoặc while để
-#tính tống các số nguyên từ 1 đến n. (n <= 1000)
-n = int(input())
-Sum = 0
-for i in range(1 , n+1) :
-  Sum += i
-print(Sum)
+#w4A2
+#[PrimeNumber]
+#VIết chương trình nhập vào một số tới khi được một số nguyên dương thì thôi.
+#Kiểm tra số này có phải là số nguyên tố hay không?
+while True :
+  n = int(input())
+  if n > 0 :
+    break
+  else :
+    print("Vui long nhap so nguyen duong")
+#Kiểm tra số này có phải số nguyên tố hay không
+if n > 2:
+  flag = True
+  for i in range(2 , int(n ** 0.5) + 1):
+     if n % i == 0:
+      flag = False
+      break
+  if flag :
+    print(f'{n} la so nguyen to')
+  else :
+    print(f'{n} khong la so nguyen to')
+else :
+  print(f'{n} khong la so nguyen to')
+
+
