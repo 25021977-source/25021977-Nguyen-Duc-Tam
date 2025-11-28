2289a3cfd0e5ea4104472691ae705ce2541bd6fe
25021977-source <25021977@vnu.edu.vn>
2025-11-28 22:01:25 +0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,12 +1,21 @@
-#Bài 20: Nhập vào số nguyên dương n, đếm xem n có bao nhiêu chữ số là số nguyên tố.
-n = int(input())
-count = 0
-if n > 0 :
-  while n > 0 :
-    a = n % 10
-    if a in (2 , 3 , 5 ,7):
-      count += 1
-    n //= 10
-  print(f'{count} so nguyen to')
-else :
-  print("vui long nhap so nguyen duong")
+#Bài 19: Dãy số fibonacci là dãy số được định nghĩa như sau: 1, 1, 2, 3, 5, 8, 13,... với số kế tiếp sẽ bằng tổng hai số trước đó  
+#Nhập vào số nguyên dương a, hãy tìm số trong dãy số fibonacci lớn nhất nhưng không vượt quá a
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+a = int(input("Nhap so nguyen duong a: "))
+
+if a <= 0:
+    print("Nhap lai")
+else:
+    n = 0
+    while True:
+        f = fib(n)
+        if f > a:
+            print(f"So can tim: {fib(n-1)}")
+            break   
+        n += 1
