8647e1f9d899644d8cca0e5d759f9f9c4f955b13
25021977-source <25021977@vnu.edu.vn>
2025-12-02 08:11:51 +0700

    Update btt4.py

--- btt4.py
+++ btt4.py
@@ -1,7 +1,21 @@
-#w4A5
-n = int(input("Nhap so phan tu n :"))
-a = list(map(int,input().split()))
-if 42 in a :
-    print("I've found the meaning of life!")
-else :
-    print("It's a joke!")
+#w4A7
+#"[LargestPrimeFactor]
+#Nhập vào số nguyên dương n (n ≥ 2). Hãy in ra ước số nguyên tố lớn nhất của n." 
+import math
+def nguyen_to(n):
+    if n < 2 :
+        return False
+    for i in range(2 , math.isqrt(n) + 1):
+        if n % i == 0:
+         return False
+    return True
+n = int(input())
+if n >= 2:
+ ans = n
+ while ans >= 2:
+    if n % ans == 0 and nguyen_to(ans):
+     print(ans)
+     break
+    ans -= 1
+
+   
