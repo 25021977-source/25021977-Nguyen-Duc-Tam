f6e24b168595144f09209d8e9c0b13a8dc5845c6
25021977-source <25021977@vnu.edu.vn>
2025-12-05 22:21:14 +0700

    Update btt5.py

--- btt5.py
+++ btt5.py
@@ -1,14 +1,15 @@
-#Nhập vào 1 dãy số đến khi nhập được 1 số âm hoặc >= 10 thì dừng .
-#liệt kê số lần xuất hiện của các số đã nhập tăng dần của số
-#không được dùng mảng quá 10 phần tử
-count = [0] * 10
-while True :
-    n = int(input())
-    if n < 0 or n >= 10 :
-        break
-    count[n] += 1
-for i in range(10) :
-     if count[i] > 0:
-       print(i , count[i])
-
+#W5A10 Viết hàm trả về \(true\) nếu hai từ \(a\) và \(b\) là đẳng cấu, ngược lại \(false\).
+# Bạn có thể giả sử hai từ đầu vào có cùng độ dài. 
+def dang_cau(a , b):
+ x = [0] * 256
+ x_1 = [0] * 256
+ for i in range(len(a)) :
+   if x[ord(a[i])] != x_1[ord(b[i])] :
+    return False
+   x[ord(a[i])] = i + 1
+   x_1[ord(b[i])] = i + 1
+ return True
+x = input()
+y = input()
+print(dang_cau(x , y))
 
