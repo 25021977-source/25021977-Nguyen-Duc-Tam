f35ab2d4963308826c81372265a196eba3bffafa
25021977-source <25021977@vnu.edu.vn>
2025-12-05 21:50:11 +0700

    Update btt5.py

--- btt5.py
+++ btt5.py
@@ -1,12 +1,20 @@
-#W5A6 Viết hàm tính giai thừa. Nhập vào số nguyên n, trả về giai thừa của n (n!) 
-def giai_thua( n ) :
-    if n < 0 :
-        return False 
-    s = 1
-    for i in range(1 , n + 1 ):
-        s *= i
-        i += 1
-    return s
-
-a = int(input())
-print(giai_thua(a))
+#W5A7 
+#Đầu vào: Hai số thực và phép toán cần thực hiện (trong số các phép toán \(+, -, *, /\)).
+#    Đầu ra: Kết quả phép toán làm tròn chữ số thập phân thứ  2
+def tinh_toan(a , b , phep_toan):
+    if phep_toan == '+' :
+        return round(a + b , 2)    #round : làm tròn đến chữ số
+    elif phep_toan == '-' :
+        return round(a - b , 2)
+    elif phep_toan == '*' :
+        return round(a * b , 2)
+    elif phep_toan == '/' :
+        if b == 0:
+            return False
+        else :
+            return round(a / b , 2)
+x = float(input("Nhap x ="))
+y = float(input("Nhap y ="))
+phep_toan = input("Nhap phep toan ")
+ket_qua = tinh_toan(x , y , phep_toan)
+print(ket_qua)
