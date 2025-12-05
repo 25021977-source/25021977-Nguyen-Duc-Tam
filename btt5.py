866c362d6565d2b38a2bac64221332d43bc25bdd
25021977-source <25021977@vnu.edu.vn>
2025-12-05 21:50:50 +0700

    Update btt5.py

--- btt5.py
+++ btt5.py
@@ -1,20 +1,19 @@
-#W5A7 
-#Đầu vào: Hai số thực và phép toán cần thực hiện (trong số các phép toán \(+, -, *, /\)).
-#    Đầu ra: Kết quả phép toán làm tròn chữ số thập phân thứ  2
-def tinh_toan(a , b , phep_toan):
-    if phep_toan == '+' :
-        return round(a + b , 2)    #round : làm tròn đến chữ số
-    elif phep_toan == '-' :
-        return round(a - b , 2)
-    elif phep_toan == '*' :
-        return round(a * b , 2)
-    elif phep_toan == '/' :
-        if b == 0:
-            return False
-        else :
-            return round(a / b , 2)
-x = float(input("Nhap x ="))
-y = float(input("Nhap y ="))
-phep_toan = input("Nhap phep toan ")
-ket_qua = tinh_toan(x , y , phep_toan)
-print(ket_qua)
+#W5A8
+#Cho hai số nguyên x và y, hãy viết hàm trả về khoảng cách Hamming giữa hai số này.
+# Khoảng cách Hamming giữa hai số nguyên là số lượng vị trí khác nhau giữa hai dãy bits tương ứng của chúng 
+def Hamming(a , a_1 ):
+    a = format(a ,'b')        #format(a,'b') : chuyển số nguyên sang nhị phân
+    a_1 = format(a_1 ,'b')
+    max_len =max(len(a) , len(a_1))
+    a = a.rjust(max_len,'0')           #rjust : thêm kí tự vào phần đầu để đạt độ dài mong muốn
+    a_1 = a_1.rjust(max_len,'0')
+
+    count = 0
+    for i in range(max_len) :
+        if a[i] != a_1[i] :
+            count += 1
+    
+    return c
+x = int(input())
+y = int(input())
+print(Hamming(x,y))
