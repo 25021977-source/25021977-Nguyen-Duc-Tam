ace64596a59203d7a0254b02eb6e6b4ae8e8c144
25021977-source <25021977@vnu.edu.vn>
2025-12-05 21:52:56 +0700

    Update btt5.py

--- btt5.py
+++ btt5.py
@@ -1,12 +1,14 @@
-#W5A9 Viết hàm trả về tổng các chữ số của số nguyên dương n
-def tong_duong(n) :
-    n = abs(n)       #abs : hàm lấy trị tuyệt đối
-    tong = 0
-    while n > 0:
-        tong += n % 10
-        n //= 10
-    return tong
+#Nhập vào 1 dãy số đến khi nhập được 1 số âm hoặc >= 10 thì dừng .
+#liệt kê số lần xuất hiện của các số đã nhập tăng dần của số
+#không được dùng mảng quá 10 phần tử
+count = [0] * 10
+while True :
+    n = int(input())
+    if n < 0 or n >= 10 :
+        break
+    count[n] += 1
+for i in range(10) :
+     if count[i] > 0:
+       print(i , count[i])
 
-n = int(input())
-print(tong_duong(n))
-        
+
