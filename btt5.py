6f931ec7cd864eab0b84c28c0f3c0be978ad53ca
25021977-source <25021977@vnu.edu.vn>
2025-12-05 21:46:42 +0700

    Update btt5.py

--- btt5.py
+++ btt5.py
@@ -1,7 +1,16 @@
-#W5A2 Viết hàm hoán đổi hai số nguyên. def swap(a, b) -> Tuple[int, int]: 
-def swap( x , y ) :
-    return y , x 
+#W5A3 Viết hàm kiểm tra một số có phải là số nguyên tố hay không? 
+import math
+def so_nguyen_to(n) :
+    if n < 2 :
+        return False
+    for i in range(2 , math.isqrt(n) + 1 ) :       # isqrt : căn bậc 2
+        if n % i == 0 :
+            return False
+    return True
 
 a = int(input())
-b = int(input())
-print(swap(a , b))
+if so_nguyen_to(a) :
+    print(f"{a} la so nguyen to")
+else :
+    print(f"{a} khong la so nguyen to")
+    
