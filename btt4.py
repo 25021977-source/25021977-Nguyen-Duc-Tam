de1620a980fe15511c358ba8f291e6ad8e9afa6c
25021977-source <25021977@vnu.edu.vn>
2025-12-06 15:34:24 +0700

    Update btt4.py

--- btt4.py
+++ btt4.py
@@ -1,12 +1,16 @@
-#W4A12
-#[Interest]
-#Một người có tài khoản tiết kiệm ở ngân hàng và gửi vào X đồng với lãi suất là 0.7% mỗi tháng.
-#Viết chương trình tính số tiền sau N tháng người ấy rút được 
-#(cả gốc và lãi, bỏ qua phần lẻ thập phân).  
-def tien_gui(X , N) :
-    lai_suot = 7 / 1000
-    so_tien = X * ( 1 + lai_suot) ** N
-    return int(so_tien)
-A = int(input("Nhap so tien :"))
-n = int(input("Nhap so thang :"))
-print(tien_gui(A ,n))
+#W4A13
+#Nhập hai số từ bàn phím và kiểm tra xem chúng có phải là cặp số thân thiết không.
+#Nếu có, in ra màn hình "True", ngược lại, in ra "False" 
+def than_thiet(a , b):
+    count = 0
+    count_1 = 0
+    for i in range(1 , a) :
+        if a % i == 0:
+            count += i
+    for j in range(1 , b) :
+        if b % j == 0 :   
+            count_1 += j
+    return count == b and count_1 == a
+x = int(input())
+y = int(input())
+print(than_thiet(x , y))
