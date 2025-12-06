26893cdb0aeb731f261f685d45e62f8f5b84e241
25021977-source <25021977@vnu.edu.vn>
2025-12-06 16:22:52 +0700

    Update btt4.py

--- btt4.py
+++ btt4.py
@@ -1,16 +1,21 @@
-#W4A13
-#Nhập hai số từ bàn phím và kiểm tra xem chúng có phải là cặp số thân thiết không.
-#Nếu có, in ra màn hình "True", ngược lại, in ra "False" 
-def than_thiet(a , b):
-    count = 0
-    count_1 = 0
-    for i in range(1 , a) :
-        if a % i == 0:
-            count += i
-    for j in range(1 , b) :
-        if b % j == 0 :   
-            count_1 += j
-    return count == b and count_1 == a
-x = int(input())
-y = int(input())
-print(than_thiet(x , y))
+#w4A6
+#"[SumPrimesInRange]
+#Nhập hai số nguyên dương a, b (a ≤ b). 
+#Hãy tính tổng các số nguyên tố trong đoạn\[a, b]. Mỗi dòng là một bộ test (a b)."
+import math
+def nguyen_to(n) :
+    if n < 2 :
+        return False
+    for i in range(2 , math.isqrt(n) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+a = int(input())
+b = int(input())
+sum = 0
+for j in range(a , b + 1):
+    if nguyen_to(j) :
+        sum += j
+
+print(sum)
