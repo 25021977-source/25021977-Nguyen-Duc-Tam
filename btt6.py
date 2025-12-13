8e6af0102ee7f9400fe9713574ecbd1aa38abb5a
25021977-source <25021977@vnu.edu.vn>
2025-12-13 21:56:26 +0700

    Update btt6.py

--- btt6.py
+++ btt6.py
@@ -1,20 +1,16 @@
-#W6A10
-#Viết chương trình nhập vào một danh sách các số nguyên, và số nguyên
-#dương k. In ra list gồm các cặp số khác nhau có tổng bằng k. Mỗi cặp là
-#một tuple, hai số trong hai tuple có thể giống nhau, thứ tự xuất hiện trong danh
-#sách của số thứ nhất nhỏ hơn thứ tự của số thứ 2 tuple trong list được
-#sắp xếp theo thứ tự tăng dần của phần tử đầu tiên trong tuple
-my_list = list(map(int,input().split()))
-k = int(input())
-output_list = []
-for i in range(len(my_list)):
-    for j in range(i + 1 , len(my_list)) :
-        if my_list[i] + my_list[j] == k :
-            output_list.append((my_list[i] , my_list[j]))
-
-for x in range(len(output_list)) :
-    for y in range(len(output_list) - 1 - x) :
-        if output_list[y][0] > output_list[y+1][0] :
-            output_list[y] , output_list[y+1] = output_list[y+1] , output_list[y]
-
-print(output_list)
+#W6A11
+#[Tách số chẵn và lẻ từ tuple]
+#Nhập vào một tuple chứa các số nguyên. Hãy tạo ra hai tuple mới: một chứa
+#tất cả các số chẵn và một chứa tất cả các số lẻ từ tuple ban đầu, giữ nguyên
+#thứ tự tương đối. In ra hai tuple này trên hai dòng riêng biệt
+my_tuple = tuple(map(int,input().split()))
+output_1 = []
+output_2 = []
+my_list = list(my_tuple)
+for i in my_list :
+    if i % 2 == 0:
+        output_1.append(i)
+    else :
+        output_2.append(i)
+print(tuple(output_1))
+print(tuple(output_2))
