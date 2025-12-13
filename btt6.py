08eb2d4689c7ccbd126bc54e06e3993b5b9558a0
25021977-source <25021977@vnu.edu.vn>
2025-12-13 21:59:11 +0700

    Update btt6.py

--- btt6.py
+++ btt6.py
@@ -1,23 +1,11 @@
-#W6A12
-#[Tìm phần tử xuất hiện nhiều nhất]
-#Nhập vào một list các số nguyên. Hãyt trong số đó. tìm số xuất hiện nhiều lần nhất trong list.
-#Nếu có nhiều số cùng có số lần xuất hiện nhiều nhất, hãy in ra số nhỏ nhất
-my_list = list(map(int,input().split()))
+#W6A13
+#[Đảo ngược key và value của dictionary]
+#Nhập vào một dictionary có value là duy nhất (không trùng nhau). Hãy tạo
+#một dictionary mới bằng cách đảo ngược key và value của dictionary ban
+#đầu. In ra dictionary mới.
+my_list = input().split()
 dic = {}
-for i in my_list :
-    if i in dic :
-        dic[i] += 1
-    else :
-        dic[i] = 1
-
-max_count = 0
-for key , value in dic.items() :     #for key , value in dic.items() :duyet tung cay value trong dic
-    if value > max_count :
-        max_count = value
-
-output_list = []
-for key , value in dic.items() :
-    if value == max_count :
-        output_list.append(key)
-
-print(min(output_list))
+for item in my_list :
+    key , value = item.split(':')
+    dic[value] = key
+print(dic)
