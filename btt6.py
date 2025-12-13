b2f8fa7458a748cac3ae0aa3d1d56381de5bfb54
25021977-source <25021977@vnu.edu.vn>
2025-12-13 17:47:25 +0700

    Update btt6.py

--- btt6.py
+++ btt6.py
@@ -1,7 +1,5 @@
-#W6A6
-#Viết chương trình nhập vào một danh sách các số nguyên, sau đó in ra tổng các số trong list đó.
-my_list = list(map(int,input().split()))
-sum = 0
-for i in my_list:
-    sum += i
-print(sum)
+#W6A7
+#Viết chương trình nhập vào một tuple, in ra phần tử đầu, phần tử cuối, và tuple đảo ngược
+my_tuple = tuple(map(int,input().split()))
+x = my_tuple[::-1]            # n[::-1] : duyệt tuple từ cuối về đầu
+print(my_tuple[0] , my_tuple[len(my_tuple) - 1] , x)
