7df0e09ac96675c61d13b40f874a12d44cdae622
25021977-source <25021977@vnu.edu.vn>
2025-12-13 17:46:35 +0700

    Update btt6.py

--- btt6.py
+++ btt6.py
@@ -1,16 +1,7 @@
-#W6A5
-#Nhập một list số nguyên, 
-#In ra dictionary {'positives': ..., 'negatives': ..., 'zeros': ...} (đếm số >0, <0, =0)
+#W6A6
+#Viết chương trình nhập vào một danh sách các số nguyên, sau đó in ra tổng các số trong list đó.
 my_list = list(map(int,input().split()))
-count = 0
-count_1 = 0
-count_2 = 0
-for i in my_list :
-    if i > 0 :
-        count += 1
-    if i < 0 :
-        count_1 += 1
-    if i == 0 :
-        count_2 += 1
-dic = {'positives' : count , 'negative' : count_1, 'zeros' : count_2}
-print(dic)
+sum = 0
+for i in my_list:
+    sum += i
+print(sum)
