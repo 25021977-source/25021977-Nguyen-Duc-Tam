78078cda63a1b185304a18fe468249203528e3a1
25021977-source <25021977@vnu.edu.vn>
2025-12-27 09:58:10 +0700

    Update btt6.py

--- btt6.py
+++ btt6.py
@@ -1,13 +1,15 @@
-#W6A20
-#Viết hàm đưa vào một list số nguyên và một số nguyên dương k. Hãy tìm và
-#trả về vị trí của phần tử đầu tiên có giá trị k trong list, nếu không có thì trả về -1
-
-def vi_tri(my_list , k) :   
+#W6A22
+#Viết hàm đưa vào 1 list số nguyên, tìm và trả về vị trí có giá trị lớn nhất trong list
+def max_index(my_list) :
+    max_count = my_list[0]
+    max_index = 0
     for i in range(len(my_list)) :
-        if my_list[i] == k :
-               return i
-    return -1
+        if my_list[i] > max_count :
+            max_count = my_list[i] 
+            max_index = i
+    return max_index
+
 my_list1 = list(map(int,input().split()))
-k1 = int(input())     
-print(vi_tri(my_list1 , k1))
+print(max_index(my_list1))
+
 
