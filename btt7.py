1617d0069612dfbb39486c3cc3d418c01d18fc79
25021977-source <25021977@vnu.edu.vn>
2025-12-27 10:10:51 +0700

    Update btt7.py

--- btt7.py
+++ btt7.py
@@ -1,21 +1,10 @@
-#W7A4
-#Viết hàm tìm phần tử có số lần xuất hiện nhiều nhất trong danh sách. Nếu có
-#nhiều phần tử cùng số lần, trả về phần tử xuất hiện trước.
-def max_idex(arr) :
-    dic = {}
-    for i in arr :
-        if i in dic :
-            dic[i] += 1
-        else :
-            dic[i] = 1
-    max_count = 0
-    for key , value in dic.items():
-        if value > max_count :
-            max_count = value 
-    output = []
-    for key , value in dic.items():
-        if max_count == value :
-            output.append(key)
-    return f"{output[0]} xuat hien nhieu nhat, som nhat,{max_count} lan"
-my_list = list(map(int,input().split()))
-print(max_idex(my_list))
+#W7A5
+#Cho danh sách các số nguyên và một số X, đếm tất cả các cặp (a, b) sao cho a + b = X
+arr = list(map(int,input().split()))
+x = int(input())
+so_cap = 0
+for i in range(len(arr)) :
+    for j in range(i+1 , len(arr)) :
+        if arr[i] + arr[j] == x :
+            so_cap += 1
+print(so_cap)
