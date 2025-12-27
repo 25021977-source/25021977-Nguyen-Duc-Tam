b30e1d5db96ec7af39f9c1aa6f5151fa0f6486fb
25021977-source <25021977@vnu.edu.vn>
2025-12-27 10:10:08 +0700

    Update btt7.py

--- btt7.py
+++ btt7.py
@@ -1,27 +1,21 @@
-#W7A3
-#Viết hàm thực hiện thuật toán bubble sort, Selection Sort để sắp xếp danh sách
-#theo thứ tự tăng dần. Sắp xếp nổi bọt (Bubble Sort); Sắp xếp chọn (Selection Sort)
-def bubble_sort(arr) :
-    n = len(arr)
-    for i in range(n) :
-        swapped = False
-        for j in range(n - i - 1) :
-            if arr[j] > arr[j+1] :
-                arr[j] , arr[j+1] = arr[j+1] , arr[j]
-                swapped = True
-        if not swapped :
-            break
-    return arr
-def selection_sort(arr) :
-    n = len(arr)
-    for i in range(n):
-        min_id = i
-        for j in range(i+1 , n):
-            if arr[j] < arr[min_id]:
-                min_id = j
-        arr[i] , arr[min_id] = arr[min_id] , arr[i]
-    return arr
-arr1 = list(map(int,input().split()))
-arr2 = list(map(int,input().split()))
-print(bubble_sort(arr1))
-print(selection_sort(arr2))
+#W7A4
+#Viết hàm tìm phần tử có số lần xuất hiện nhiều nhất trong danh sách. Nếu có
+#nhiều phần tử cùng số lần, trả về phần tử xuất hiện trước.
+def max_idex(arr) :
+    dic = {}
+    for i in arr :
+        if i in dic :
+            dic[i] += 1
+        else :
+            dic[i] = 1
+    max_count = 0
+    for key , value in dic.items():
+        if value > max_count :
+            max_count = value 
+    output = []
+    for key , value in dic.items():
+        if max_count == value :
+            output.append(key)
+    return f"{output[0]} xuat hien nhieu nhat, som nhat,{max_count} lan"
+my_list = list(map(int,input().split()))
+print(max_idex(my_list))
