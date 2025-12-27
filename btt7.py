0c5d7906171117cef549e0d062c1c65ccf6a97c6
25021977-source <25021977@vnu.edu.vn>
2025-12-27 10:06:07 +0700

    Update btt7.py

--- btt7.py
+++ btt7.py
@@ -1,13 +1,27 @@
-#W7A2
-#Cho danh sách arr và một số nguyên x, đếm xem x xuất hiện bao nhiêu lần
-# trong danh sách. def count_occurrences(arr, x) - > int:
-def count_occurrences(arr , x) :
-    sum = 0
-    for i in range(len(arr)):
-        if arr[i] == x :
-            sum += 1
-    return sum
-               
-my_list = list(map(int,input().split()))
-k = int(input())
-print(count_occurrences(my_list , k))
+#W7A3
+#Viết hàm thực hiện thuật toán bubble sort, Selection Sort để sắp xếp danh sách
+#theo thứ tự tăng dần. Sắp xếp nổi bọt (Bubble Sort); Sắp xếp chọn (Selection Sort)
+def bubble_sort(arr) :
+    n = len(arr)
+    for i in range(n) :
+        swapped = False
+        for j in range(n - i - 1) :
+            if arr[j] > arr[j+1] :
+                arr[j] , arr[j+1] = arr[j+1] , arr[j]
+                swapped = True
+        if not swapped :
+            break
+    return arr
+def selection_sort(arr) :
+    n = len(arr)
+    for i in range(n):
+        min_id = i
+        for j in range(i+1 , n):
+            if arr[j] < arr[min_id]:
+                min_id = j
+        arr[i] , arr[min_id] = arr[min_id] , arr[i]
+    return arr
+arr1 = list(map(int,input().split()))
+arr2 = list(map(int,input().split()))
+print(bubble_sort(arr1))
+print(selection_sort(arr2))
