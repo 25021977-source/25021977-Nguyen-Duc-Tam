1fbe39617d762cbe70d669e0a4145a77abcacef1
25021977-source <25021977@vnu.edu.vn>
2025-12-27 10:06:54 +0700

    Update btt6.py

--- btt6.py
+++ btt6.py
@@ -1,13 +1,19 @@
-#W6A23
-#VIết hàm xóa đi các số < x có trong mảng
-def remove(my_list , x) :
+#W6A27
+#Cho một dãy số nguyên, Tìm và in ra vị trí các số < x (x là giá trị cho trước).
+#nếu không có in ra -1
+def vi_tri(my_list , x) :
     output_list = []
-    for i in my_list :
-        if i >= x :
+    for i in range(len(my_list)) :
+        if my_list[i] < x :
             output_list.append(i)
+    if not output_list :
+        return -1
     return output_list
+        
+    
+    
 my_list1 = list(map(int,input().split()))
 k = int(input())
-print(remove(my_list1 , k))
+print(vi_tri(my_list1 , k))
 
 
