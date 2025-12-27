2c4aed6de775ef2e70403627bf26faa3401933fa
25021977-source <25021977@vnu.edu.vn>
2025-12-27 10:08:09 +0700

    Update btt6.py

--- btt6.py
+++ btt6.py
@@ -1,19 +1,20 @@
-#W6A27
-#Cho một dãy số nguyên, Tìm và in ra vị trí các số < x (x là giá trị cho trước).
-#nếu không có in ra -1
-def vi_tri(my_list , x) :
-    output_list = []
-    for i in range(len(my_list)) :
-        if my_list[i] < x :
-            output_list.append(i)
-    if not output_list :
-        return -1
-    return output_list
+#W6A28
+#Cho một dãy các số nguyên, kiểm tra xem nó là dãy tăng thực sự hay giảm
+#thực sự, hay vô danh (không tăng, không giảm)
+my_list = list(map(int,input().split()))
+tang = True
+giam = True
+for i in range(len(my_list) - 1):
+    if my_list[i] >= my_list[i+1]:
+        tang = False
         
-    
-    
-my_list1 = list(map(int,input().split()))
-k = int(input())
-print(vi_tri(my_list1 , k))
+    if my_list[i] <= my_list[i+1]:
+        giam = False
+if tang :
+    print('Tang')
+elif giam :
+    print("Giam")
+else :
+    print("Vo danh")
 
 
